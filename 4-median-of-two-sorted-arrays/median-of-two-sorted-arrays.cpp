cef3a35ace7909197d53eab6a68896048a5df85f
Hen00af <hs.hs.5617.1123@gmail.com>
2026-04-10 13:43:40 +0900

    Time: 0 ms (100.00%) | Memory: 95 MB (94.32%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,17 +1,44 @@
+#include <vector>
+#include <climits>
+using namespace std;
+
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> all_nums = nums1;
-        
+    double findMedianSortedArrays(vector<int>& A, vector<int>& B) {
 
-        all_nums.insert(all_nums.end(), nums2.begin(), nums2.end());
-        sort(all_nums.begin(), all_nums.end());
-        int n = all_nums.size();
+        // Make sure A is smaller
+        if (A.size() > B.size()) return findMedianSortedArrays(B, A);
 
-        if (n % 2)
-            return all_nums[n / 2];
-        else
-            return (double)(all_nums[n / 2] +  all_nums[n / 2 - 1] ) / 2;
+        int m = A.size(), n = B.size();
+        int left = 0, right = m;
 
+        while (left <= right) {
+
+            int i = (left + right) / 2;              // cut in A
+            int j = (m + n + 1) / 2 - i;            // cut in B
+
+            int Aleft  = (i == 0) ? INT_MIN : A[i-1];
+            int Aright = (i == m) ? INT_MAX : A[i];
+
+            int Bleft  = (j == 0) ? INT_MIN : B[j-1];
+            int Bright = (j == n) ? INT_MAX : B[j];
+
+            // Correct partition
+            if (Aleft <= Bright && Bleft <= Aright) {
+
+                if ((m + n) % 2 == 0)
+                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2.0;
+                else
+                    return max(Aleft, Bleft);
+            }
+
+            // Move left or right
+            else if (Aleft > Bright)
+                right = i - 1;
+            else
+                left = i + 1;
+        }
+
+        return 0.0;
     }
 };
