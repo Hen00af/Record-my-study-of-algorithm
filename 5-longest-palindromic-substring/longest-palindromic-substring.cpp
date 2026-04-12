c6251b3b2d27cb218632faf346f83d5d11b8f76b
Hen00af <hs.hs.5617.1123@gmail.com>
2026-04-12 10:47:30 +0900

    Time: 83 ms (37.04%) | Memory: 164.8 MB (23.23%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
-        if (n < 2) return s;
+        if (s.empty()) return "";
+        int start = 0, maxLen = 0;
 
-        // DPテーブル: dp[i][j] は s[i...j] が回文かどうか
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        int start = 0;
-        int maxLen = 1;
+        for (int i = 0; i < s.size(); i++) {
+            // 1. 中心が1文字の場合 (aba)
+            int len1 = expand(s, i, i);
+            // 2. 中心が2文字の間の場合 (abba)
+            int len2 = expand(s, i, i + 1);
 
-        // 全ての1文字は回文
-        for (int i = 0; i < n; ++i) dp[i][i] = true;
-
-        // 長さ2から順にチェック（diff + 1 が現在の長さ）
-        for (int len = 2; len <= n; ++len) {
-            for (int i = 0; i <= n - len; ++i) {
-                int j = i + len - 1;
-                
-                if (s[i] == s[j]) {
-                    if (len == 2 || dp[i + 1][j - 1]) {
-                        dp[i][j] = true;
-                        if (len > maxLen) {
-                            start = i;
-                            maxLen = len;
-                        }
-                    }
-                }
+            int len = max(len1, len2);
+            if (len > maxLen) {
+                // 最長を更新したら、その開始位置を計算して保存
+                start = i - (len - 1) / 2;
+                maxLen = len;
             }
         }
-
         return s.substr(start, maxLen);
+    }
+
+private:
+    int expand(string s, int l, int r) {
+        while (l >= 0 && r < s.size() && s[l] == s[r]) {
+            l--;
+            r++;
+        }
+        return r - l - 1;
     }
 };
