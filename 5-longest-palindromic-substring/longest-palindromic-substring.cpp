d14e2d173f6df0e0163e2eea7602bd11db42a135
Hen00af <hs.hs.5617.1123@gmail.com>
2026-04-12 10:47:59 +0900

    Time: 77 ms (42.27%) | Memory: 164.7 MB (24.87%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -5,14 +5,11 @@ public:
         int start = 0, maxLen = 0;
 
         for (int i = 0; i < s.size(); i++) {
-            // 1. 中心が1文字の場合 (aba)
             int len1 = expand(s, i, i);
-            // 2. 中心が2文字の間の場合 (abba)
             int len2 = expand(s, i, i + 1);
 
             int len = max(len1, len2);
             if (len > maxLen) {
-                // 最長を更新したら、その開始位置を計算して保存
                 start = i - (len - 1) / 2;
                 maxLen = len;
             }
